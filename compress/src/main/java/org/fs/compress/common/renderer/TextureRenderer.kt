195fefecd64021f2fe9c942c42259c7b6764e4da
Fatih Sen <fatih.sen@alnoortv.com>
2019-12-28 19:29:25 +0300

    update

--- compress/src/main/java/org/fs/compress/common/renderer/TextureRenderer.kt
+++ compress/src/main/java/org/fs/compress/common/renderer/TextureRenderer.kt
@@ -170,6 +170,7 @@ class TextureRenderer: Renderer {
     GLES20.glShaderSource(shader, source)
 
     GLES20.glCompileShader(shader)
+
     val status = IntArray(1)
     GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, status, 0)
     if (status[0] != GLES20.GL_TRUE) {
